0f95c3d66e52b0a1f4bc5867bdb7f3a7baed0c11
ayman2almonster <42792992+ayman2almonster@users.noreply.github.com>
2018-11-25 01:32:14 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Type *play | by Ayman ALmonster`,"http://twitch.tv/Death Shop")
+client.user.setGame(`Type *help | by Ayman ALmonster`,"http://twitch.tv/Death Shop")
 client.user.setStatus("dnd")
 });
 
