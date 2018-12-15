2e5710007de4708f2025c4215393ccbe8bd37de5
ayman2almonster <42792992+ayman2almonster@users.noreply.github.com>
2018-12-15 20:13:13 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,7 +20,6 @@ client.on("message", message => {
   let args = message.content.split(" ").slice(1);
   var argresult = args.join(' '); 
   message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
- m.send(`${argresult}\n ${m}`);
 })
  message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'all').size}\` **: عدد الاعضاء المستلمين**`); 
  message.delete(); 
