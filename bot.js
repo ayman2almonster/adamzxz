74c43e159143a700e5b94ba6c0df2d3566b7e8d2
ayman2almonster <42792992+ayman2almonster@users.noreply.github.com>
2018-12-15 20:22:32 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,6 +18,7 @@ client.on("message", message => {
             if (message.content.startsWith(prefix + "bc")) {  ///الامر
                          if (!message.member.hasPermission("ADMINISTRATOR"))  return;
   let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
   message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
  m.send(`${argresult}\n ${m}`);
 })
@@ -25,6 +26,7 @@ client.on("message", message => {
  message.delete(); 
 };     
 });
+
 client.on('message' , message => {
 if(message.content === '*help') {
   var EsTeKnAN = new Discord.RichEmbed()
