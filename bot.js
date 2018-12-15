93a3bdec88b5dd2ccf19137beab25749215e7ec7
ayman2almonster <42792992+ayman2almonster@users.noreply.github.com>
2018-12-15 20:02:51 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,27 +13,19 @@ client.user.setGame(`Type *help | by Ayman ALmonster`,"http://twitch.tv/Death Sh
 client.user.setStatus("dnd")
 });
 
-client.on('message', message => {
-    var prefix = "*";
-   
-        if (message.author.id === client.user.id) return;
-        if (message.guild) {
-       let embed = new Discord.RichEmbed()
-        let args = message.content.split(' ').slice(1).join(' ');
-    if(message.content.split(' ')[0] == prefix + 'bc') {
-        if (!args[1]) {
-    message.channel.send("*bc <message>");
-    return;
-    }
-            message.guild.members.forEach(m => {
-       if(!message.member.hasPermission('ADMINISTRATOR')) return;
-                var bc = new Discord.RichEmbed()
-            });
-        }
-        } else {
-            return;
-        }
-    });
+client.on("message", message => {
+
+            if (message.content.startsWith(prefix + "bc")) {  ///الامر
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'all').size}\` **: عدد الاعضاء المستلمين**`); 
+ message.delete(); 
+};     
+});
 
 client.on('message' , message => {
 if(message.content === '*help') {
